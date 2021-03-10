4a6c58b7f0c1d6c4571f6db8696347da6ac607c4
Abhishek Anand <abhishek+github@hey.com>
2021-03-10 17:03:50 +0530

    Initial commit

---
+++ src/DateAndTime.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+
+class Result {
+
+    public static String findDay(int month, int day, int year) {
+        return LocalDate.of(year, month, day).getDayOfWeek().name();
+    }
+
+}
+
+public class DateAndTime {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("\\User\\abhishek\\Downloads\\newfile.txt")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = Result.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ src/EndOfFile.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class EndOfFile {
+
+    public static void main(String[] args) {
+        int count = 1;
+        try(Scanner in = new Scanner(System.in))
+        {
+            while(in.hasNext())
+            {
+                System.out.println(count + " " + in.nextLine());
+                ++count;
+            }
+        }
+    }
+}
